32a93ded8f66b57c864f03fcb30d0c0f41e507ff
HKrysiak <40641661+HKrysiak@users.noreply.github.com>
2018-08-31 16:03:10 -0400

    Example of comflict

--- Stepper Valve.py
+++ Stepper Valve.py
@@ -1,6 +1,3 @@
-Python 3.5.3 (default, Jan 19 2017, 14:11:04)
-[GCC 6.3.0 20170124] on linux
-Type "copyright", "credits" or "license()" for more information.
 >>> import time
 >>> import piplates.MOTORplate as MOTOR
 MOTOR.stepperCONFIG(0,'a','cw','F', 2000, 2)
